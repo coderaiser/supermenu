83e6a1ca5367d45f899fcf01e5e3767b9ef976a0
coderaiser <mnemonic.enemy@gmail.com>
2014-04-08 07:56:25 -0400

    feature(menu) add

---
+++ menu-data.js
@@ -0,0 +1,31 @@
+var MenuProto;
+
+(function() {
+    'use strict';
+    
+    var  menu;
+    
+    menu = new MenuProto({
+        'help': function() {
+            console.log('*help');
+        },
+        'view': function() {
+            console.log('*view');
+        },
+        'edit': function() {
+            console.log('edit');
+        },
+        'download': {
+            'github': {
+                'gist': function() {
+                    console.log('*gist');}
+                },
+                'main': function() {
+                    console.log('*main');
+                }
+            },
+            'dropbox': function() {
+                console.log('*dropbox');
+            },
+    });
+})();
---
+++ menu.js
@@ -0,0 +1,161 @@
+var MenuProto, Util;
+
+(function (window) {
+    'use strict';
+    
+    MenuProto = function(element, menuData) {
+        var ElementMenu,
+            Element,
+            ElementEvent,
+            MenuFuncs       = {},
+            MENU_ITEM_NAME  = 'js-menu-item',
+            MENU_NAME       = 'js-menu',
+            TEMPLATE        = {
+                MAIN:   '<ul id="js-menu" class="menu menu-hidden">'                    +
+                            '{{ items }}'                                               +
+                        '</ul>',
+                ITEM:   '<li id="js-menu-{{ name }}" class="menu-item{{ className }}">' +
+                            '<label>{{ name }}</label>'                                 +
+                            '{{ subitems }}'                                            +
+                        '</li>'
+            };
+        
+        if (menuData) {
+            Element         =
+            ElementEvent    = element;
+        } else {
+            Element         = document.body;
+            ElementEvent    = window;
+            menuData        = element;
+        }
+        
+        function init() {
+            var name, isObj = Util.isObject(menuData);
+            
+            if (!isObj) {
+                name            = menuData;
+                menuData        = {};
+                menuData[name]  = null;
+            }
+            
+            ElementMenu         = createMenu(menuData);
+            
+            ElementEvent.addEventListener('click', onClick);
+            ElementEvent.addEventListener('contextmenu', onContextMenu);
+        }
+        
+        function createMenu(menuData) {
+            var elementMenu,
+                menu        = '',
+                items       = '',
+                buildItems  = function(menuData) {
+                    var name, isObj, data,
+                        className   = '',
+                        subitems    = '',
+                        items       = '';
+                    
+                    for (name in menuData) {
+                        data    = menuData[name];
+                        isObj   = Util.isObject(data);
+                        
+                        if (isObj) {
+                            subitems    = Util.render(TEMPLATE.MAIN, {
+                                items: buildItems(data)
+                            });
+                            
+                            className   = ' menu-submenu';
+                        } else {
+                            MenuFuncs[name] = data;
+                        }
+                        
+                        items       += Util.render(TEMPLATE.ITEM, {
+                            'name'      : name,
+                            'subitems' : subitems,
+                            'className' : className
+                        });
+                    }
+                    
+                    return items;
+                };
+            
+            items = buildItems(menuData);
+            
+            menu = Util.render(TEMPLATE.MAIN, {
+                items: items
+            });
+            
+            Element.innerHTML   += menu;
+            elementMenu         = Element.querySelector('#js-menu');
+            
+            return elementMenu;
+        }
+        
+        this.show   = showMenuElement;
+        this.hide   = hideMenuElement;
+        
+        function onClick(event) {
+            var itemData;
+            
+            hideMenuElement();
+            
+            itemData = getMenuItemData(event.target);
+            
+            Util.exec(itemData);
+        }
+        
+        function onContextMenu(event) {
+            var itemData,
+                x   = event.x,
+                y   = event.y;
+            
+            hideMenuElement();
+            
+            ElementMenu.style.left     = x - 5 +'.px';
+            ElementMenu.style.top      = y - 5 + '.px';
+            
+            itemData = getMenuItemData(event.target);
+            
+            if (itemData)
+                Util.exec(itemData);
+            else
+                showMenuElement();
+            
+            event.preventDefault();
+        }
+        
+        function showMenuElement() {
+            ElementMenu.classList.remove('menu-hidden');
+        }
+        
+        function hideMenuElement() {
+            ElementMenu.classList.add('menu-hidden');
+        }
+        
+        function getMenuItemData(target) {
+            var label, data, parent, idParent, isLabel,
+                isMenuNameId, isMenuItemId;
+            
+            if (target) {
+                parent          = target.parentElement,
+                idParent        = parent && parent.id,
+                isMenuItemId    = idParent === MENU_ITEM_NAME,
+                isMenuNameId    = idParent === MENU_NAME,
+                isLabel         = target.tagName === 'LABEL';
+                
+                if (isMenuItemId) {
+                    data        = target.textContent;
+                } else if (isMenuNameId) {
+                    label       = target.querySelector('label');
+                    data        = label.textContent;
+                } else if(isLabel) {
+                    data        = MenuFuncs[target.textContent];
+                }
+            }
+            
+            return data;
+        }
+        
+        init();
+    };
+    
+})(window);
