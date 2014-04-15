6fc4b2cc0f6609e50f0cc704c31146d9c2a3a7e2
coderaiser <mnemonic.enemy@gmail.com>
2014-04-15 07:00:05 -0400

    feature(gulp) add jshint, recess

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,11 @@
         uglify      = require('gulp-uglify'),
         concat      = require('gulp-concat'),
         rename      = require('gulp-rename'),
-                
+        jshint      = require('gulp-jshint'),
+        recess      = require('gulp-recess'),
+        
+        NAME_CSS    = 'menu.css',
+        NAME_JS     = ['menu.js', 'util.io-part.js'],
         SUFFIX      = '.min';
         
     
@@ -20,15 +24,31 @@
             .on('error', onError);
     });
     
+    gulp.task('css-check', function () {
+        gulp.src(NAME_CSS)
+            .pipe(recess())
+            .on('error', onError);
+    });
+    
     gulp.task('js', function() {
-      gulp.src(['menu.js', 'util.io-part.js'])
+      gulp.src(NAME_JS)
         .pipe(uglify())
         .pipe(concat('menu.min.js'))
         .pipe(gulp.dest('./'))
         .on('error', onError);
     });
     
-    gulp.task('default', ['css', 'js']);
+    gulp.task('js-check', function() {
+        var namesStr    = NAME_JS.join(',') + ',gulpfile.js',
+            names       = namesStr.split(',');
+        
+        gulp.src(names)
+            .pipe(jshint())
+            .pipe(jshint.reporter())
+            .on('error', onError);
+    });
+    
+    gulp.task('default', ['css', 'css-check', 'js-check']);
     
     function onError(params) {
         console.log(params.message);
