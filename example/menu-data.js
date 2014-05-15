70233d8227f5f6a45e2a37349fd04551e7994ba5
coderaiser <mnemonic.enemy@gmail.com>
2014-05-15 09:22:04 -0400

    feature(menu) beforeShow: x, y, name -> params {x, y, name}

--- example/menu-data.js
+++ example/menu-data.js
@@ -11,38 +11,6 @@ var menu, MenuIO;
     menu = MenuIO(element, options, {
         'help': function() {
             console.log('*help');
-        },
-        'upload': {
-            'github': {
-                'gist': function() {
-                    console.log('*gist');
-                },
-                'main': function() {
-                    console.log('*main');
-                }
-            },
-            'dropbox': function() {
-                console.log('*dropbox');
-            }
-        },
-        'view': function() {
-            console.log('*view');
-        },
-        'edit': function() {
-            console.log('edit');
-        },
-       'download': {
-            'github': {
-                'gist': function() {
-                    console.log('*gist');
-                },
-                'main': function() {
-                    console.log('*main');
-                }
-            },
-            'dropbox': function() {
-                console.log('*dropbox');
-            }
-        },
+        }
     });
 })();
