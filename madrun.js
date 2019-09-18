a90e221e5e04846a1631f574f9f6f5fe35825017
coderaiser <coderaiser@cloudcmd.io>
2019-09-18 16:50:03 +0300

    feature(supermenu) drop support of dist

--- madrun.js
+++ madrun.js
@@ -5,9 +5,7 @@ const {run} = require('madrun');
 module.exports = {
     'lint': () => run('lint:*'),
     'fix:lint': () => run('lint:*', '--fix'),
-    'lint:js': () => 'putout lib madrun.js webpack.config.js',
+    'lint:js': () => 'putout lib madrun.js',
     'lint:css': () => 'stylelint css',
-    'build': () => 'webpack --progress --mode production',
-    'prepublishOnly': () => run('build'),
 };
 
