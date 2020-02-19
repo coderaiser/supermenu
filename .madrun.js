d4f8e89e9619814f83826cf2a0966a091cbd54fa
coderaiser <coderaiser@cloudcmd.io>
2020-02-19 10:52:12 +0200

    feature(supermenu) add build

--- .madrun.js
+++ .madrun.js
@@ -1,11 +1,20 @@
 'use strict';
 
 const {run} = require('madrun');
+const {version} = require('./package');
 
 module.exports = {
     'lint': () => run('lint:*'),
     'fix:lint': () => run('lint:*', '--fix'),
-    'lint:js': () => 'putout lib .madrun.js',
+    'lint:js': () => 'putout lib .madrun.js *.js',
     'lint:css': () => 'stylelint css',
+    'build': () => 'webpack --progress --mode production',
+    'wisdom': () => run(['build', 'upload:*']),
+    'upload:main': () => upload('dist/supermenu.min.js'),
+    'upload:main:map': () => upload('dist/supermenu.min.js.map'),
 };
+
+function upload(name) {
+    return `putasset -o coderaiser -r supermenu -t v${version} -f ${name}`;
+}
 
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const dir = './lib';
+
+const dist = path.resolve(__dirname, 'dist');
+const devtool = 'source-map';
+
+const rules = [{
+    test: /\.css$/,
+    loader: 'style-loader!css-loader!clean-css-loader',
+}, {
+    test: /\.(png|gif|svg|woff|woff2|eot|ttf)$/,
+    loader: 'url-loader?limit=50000',
+}];
+
+const optimization = {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        extractComments: true,
+      }),
+    ],
+}
+
+const filename = `[name].min.js`;
+
+module.exports = {
+    devtool,
+    entry: {
+        'supermenu': `${dir}/supermenu.js`,
+    },
+    optimization,
+    output: {
+        library: 'supermenu',
+        filename,
+        path: dist,
+        pathinfo: true,
+        libraryTarget: 'var',
+        devtoolModuleFilenameTemplate,
+    },
+    module: {
+        rules,
+    },
+};
+
+function devtoolModuleFilenameTemplate(info) {
+    const resource = info.absoluteResourcePath.replace(__dirname + path.sep, '');
+    return `file://supermenu/${resource}`;
+}
