4e7aefe2377f16382a9d428cce7240dab02f5728
coderiaser <coderaiser@cloudcmd.io>
2025-08-14 18:39:22 +0300

    chore: madrun: rm build step

--- .madrun.js
+++ .madrun.js
@@ -5,13 +5,5 @@ const {run} = require('madrun');
 module.exports = {
     'fix:lint': () => run('lint', '--fix'),
     'lint': () => 'putout .',
-    'build': () => 'webpack --progress --mode production',
-    'wisdom': () => run('build'),
-    'wisdom:done': () => run('upload:*'),
-    'upload:main': () => upload('dist/supermenu.min.js'),
-    'upload:main:map': () => upload('dist/supermenu.min.js.map'),
 };
 
-function upload(name) {
-    return 'putasset -o coderaiser -r supermenu -t v`version`' + ` -f ${name}`;
-}
